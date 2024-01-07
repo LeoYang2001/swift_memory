4f6d9f503e2e333b43a5183cf441e4da31fc7c3e
Leo Yang <jya261@uky.edi>
2024-01-07 18:13:31 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 杨嘉煌 on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
